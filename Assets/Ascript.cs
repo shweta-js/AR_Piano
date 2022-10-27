3fa864b635f4844b345f3c74c840cd4ae94755bf
Shweta.js <shwetajs200@gmail.com>
2022-10-27 19:03:42 +0530

    Add files via upload

---
+++ Assets/Ascript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class Ascript : MonoBehaviour
+{
+    
+    public GameObject cube;
+    public VirtualButtonBehaviour Vb;
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+
+
+    void Start()
+    {
+        Vb.RegisterOnButtonPressed(OnButtonPressed);
+        Vb.RegisterOnButtonReleased(OnButtonReleased);
+        
+       cube.SetActive(false);
+        MusicSource.clip=MusicClip;
+
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+       cube.SetActive(true);
+        MusicSource.Play();
+     
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+        cube.SetActive(false);
+    }
+}
